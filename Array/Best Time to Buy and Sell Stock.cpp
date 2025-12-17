d8abd4346d429935dfe745c31b77fb2b1aa64b25
Priya Kourav <priyakourav55@gmail.com>
2025-12-17 18:44:28 +0530

    Update Best Time to Buy and Sell Stock solution

--- Array/Best Time to Buy and Sell Stock.cpp
+++ Array/Best Time to Buy and Sell Stock.cpp
@@ -7,14 +7,21 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        // This will store the minimum stock price
         int minPrice = INT_MAX;
+
+        // This will store the maximum profit we can achieve
         int maxProfit = 0;
 \
 
-
+//loop through each price in the prices array
         for (int price : prices) {
+
+            // Agar current price chhota hai, to buy karne ka better chance
             if (price < minPrice) {
                 minPrice = price;
+
+                // Nahi to aaj sell karne par kitna profit milega, wo check kregein
             } else {
                 maxProfit = max(maxProfit, price - minPrice);
                  }
