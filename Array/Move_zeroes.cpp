0ea64a7552344ba30d2147445bc618cd06981d91
Priya Kourav <priyakourav55@gmail.com>
2025-12-21 11:52:43 +0530

    added move_zeroes file

---
+++ Array/Move_zeroes.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+ //move zeroes to end of array
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int j = 0; 
+
+
+        for (int i = 0; i < nums.size(); i++) {
+            //if i th element is not zero, then we swap it with jth element
+            if (nums[i] != 0) {
+                swap(nums[j], nums[i]);
+                j++;
+            }
+        }
+
+    
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {0, 1, 0, 3, 12}; //making an dynamic array
+
+    s.moveZeroes(nums); //calling the function
+
+    for (int x : nums) {
+        cout << x << " "; // printing each element after moving zeroes to end
+    }
+    return 0;
+}
