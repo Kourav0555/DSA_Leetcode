148750646dcd5ffab949fb19fa511f26a913dcb6
Priya Kourav <priyakourav55@gmail.com>
2026-05-23 22:38:05 +0530

    added sorting char by frequency file

---
+++ Sorting/sortchar_frequency.cpp
@@ -0,0 +1,54 @@
+//QNo.451  Sort Characters By Frequency
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string frequencySort(string s) {
+
+    unordered_map<char, int> freq;
+
+    // Count frequency
+    for(char ch : s) {
+        freq[ch]++;
+    }
+
+    // Store in vector
+    vector<pair<char, int>> vec;
+
+    for(auto x : freq) {
+        vec.push_back(x);
+    }
+
+    // Sort by frequency descending
+    sort(vec.begin(), vec.end(),
+        [](pair<char,int> a, pair<char,int> b) {
+            return a.second > b.second;
+        });
+
+    string ans = "";
+
+    // Create final string
+    for(auto x : vec) {
+
+        for(int i = 0; i < x.second; i++) {
+            ans += x.first;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+
+    string s;
+
+    cout << "Enter string: ";
+    cin >> s;
+
+    cout << "Sorted String: " << frequencySort(s);
+
+    return 0;
+}
