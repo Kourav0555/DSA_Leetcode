0a1e7d83edb9e1f9a774b9b41900f40675e8fcf7
Priya Kourav <priyakourav55@gmail.com>
2026-05-23 22:44:19 +0530

    updated fizz buzz file

--- String/fizz_buzz.cpp
+++ String/fizz_buzz.cpp
@@ -9,11 +9,13 @@ using namespace std;
 vector<string> fizzBuzz(int n) {
     vector<string> result;
     for (int i = 1; i <= n; ++i) {
+
         if (i % 3 == 0 && i % 5 == 0) {
-            result.push_back("FizzBuzz");
+
+              result.push_back("FizzBuzz");
         } else if (i % 3 == 0) {
             result.push_back("Fizz");
-        } else if (i % 5 == 0) {
+        }  else if (i % 5 == 0) {
             result.push_back("Buzz");
         } else {
             result.push_back(to_string(i));
@@ -21,7 +23,6 @@ vector<string> fizzBuzz(int n) {
     }
     return result;
 }
-
 
 
 int main() {
